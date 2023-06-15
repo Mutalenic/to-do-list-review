193878fdbbc6dc6090e61653e7eb6206fead6480
Nicholas Mutale <nicomutale@gmail.com>
2023-06-15 22:40:43 +0000

    update tailwind config

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className="bg-gray-200 min-h-screen flex items-center justify-center">
+      <header className="text-center">
+        <h1 className="text-3xl font-bold mb-4">Todo List</h1>
+        <p className="text-gray-600">Start building your todo list using Tailwind CSS!</p>
         <a
-          className="App-link"
+          className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded mt-4 inline-block"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
