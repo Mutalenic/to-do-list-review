8c50d161553d691c5d537b7b854cf94a9e83fb7a
Nicholas Mutale <nicomutale@gmail.com>
2023-06-15 22:54:43 +0000

    import todolist component in app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from 'react';
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="bg-gray-200 min-h-screen flex items-center justify-center">
-      <header className="text-center">
+      <div className="bg-white shadow-lg rounded p-8">
         <h1 className="text-3xl font-bold mb-4">Todo List</h1>
-        <p className="text-gray-600">Start building your todo list using Tailwind CSS!</p>
-        <a
-          className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded mt-4 inline-block"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <TodoList />
+      </div>
     </div>
   );
 }
-
-export default App;
