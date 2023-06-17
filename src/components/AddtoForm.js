1bf212fa3bbabec9aede75cc2e9bf1814144f275
Nicholas Mutale <nicomutale@gmail.com>
2023-06-17 20:26:10 +0000

    Update addtoform.js

--- src/components/AddtoForm.js
+++ src/components/AddtoForm.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function AddtoForm() {
-  
+function AddTodoForm({ addTodo }) {
+  const [newTodo, setNewTodo] = useState('');
+
+  // Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (newTodo.trim() === '') return; // Prevent adding empty todos
+
+    addTodo(newTodo);
+    setNewTodo('');
+  };
+
   return (
-      
-      <p>
-        AddtoForm
-      </p>
+    <form onSubmit={handleSubmit} className="mb-4">
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        className="flex-1 rounded-l py-2 px-4"
+        placeholder="Enter a new todo..."
+      />
+      <button
+        type="submit"
+        className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-r"
+      >
+        Add
+      </button>
+    </form>
   );
 }
 
-export default AddtoForm;
+export default AddTodoForm;
