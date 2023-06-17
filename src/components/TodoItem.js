3fc71a638a8cc126a870d7bf11f63fa9b2fb0f1a
Nicholas Mutale <nicomutale@gmail.com>
2023-06-17 20:23:40 +0000

    update itemtodo

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,36 @@
 import React from 'react';
 
-function TodoItem() {
-  
+function TodoItem({ todo, removeTodo, markComplete }) {
+  // Handle checkbox change
+  const handleCheckboxChange = () => {
+    markComplete(todo.id);
+  };
+
+  // Handle delete button click
+  const handleDeleteClick = () => {
+    removeTodo(todo.id);
+  };
+
   return (
-      
-      <p>
-        TodoItem
+    <div className="flex items-center mb-2">
+      <input
+        type="checkbox"
+        checked={todo.completed}
+        onChange={handleCheckboxChange}
+        className="mr-2"
+      />
+      <p
+        className={`flex-1 ${todo.completed ? 'line-through text-gray-500' : ''}`}
+      >
+        {todo.text}
       </p>
+      <button
+        onClick={handleDeleteClick}
+        className="text-red-600 hover:text-red-800"
+      >
+        Delete
+      </button>
+    </div>
   );
 }
 
